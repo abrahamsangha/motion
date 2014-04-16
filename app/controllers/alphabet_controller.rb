272814c8f8267ffd2df8df77953198e27712c0d0
Abraham Sangha <abraham.sangha@gmail.com>
2014-04-15 19:36:26 -0500

    Added touch enabled feature

--- app/controllers/alphabet_controller.rb
+++ app/controllers/alphabet_controller.rb
@@ -6,10 +6,12 @@ class AlphabetController < UIViewController
     @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
     self.view.addSubview(@table)
     @table.dataSource = self
+    @table.delegate = self
+    @data = ("A".."Z").to_a
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
-    10
+    @data.count
   end
 
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
@@ -19,7 +21,24 @@ class AlphabetController < UIViewController
     cell ||= UITableViewCell.alloc.initWithStyle(
       UITableViewCellStyleDefault,
       reuseIdentifier:@reuseIdentifier)
-
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
+    cell.textLabel.text = @data[indexPath.row]
     cell
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath: indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    letter = @data[indexPath.row]
+
+    controller = UIViewController.alloc.initWithNibName(nil, bundle:nil)
+    controller.view.backgroundColor = UIColor.whiteColor
+    controller.title = letter
+
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = letter
+    label.sizeToFit
+    label.center = [controller.view.frame.size.width / 2, controller.view.frame.size.height / 2]
+    controller.view.addSubview(label)
+    self.navigationController.pushViewController(controller, animated:true)
   end
 end
