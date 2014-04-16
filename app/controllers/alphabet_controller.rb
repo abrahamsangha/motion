5ebfa942bd778208b6b5eee18a541deabc91a2e6
Abraham Sangha <abraham.sangha@gmail.com>
2014-04-15 19:05:15 -0500

    WIP tableView

--- app/controllers/alphabet_controller.rb
+++ app/controllers/alphabet_controller.rb
@@ -5,5 +5,21 @@ class AlphabetController < UIViewController
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
     self.view.addSubview(@table)
+    @table.dataSource = self
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    10
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+    cell ||= UITableViewCell.alloc.initWithStyle(
+      UITableViewCellStyleDefault,
+      reuseIdentifier:@reuseIdentifier)
+
+    cell
   end
 end
